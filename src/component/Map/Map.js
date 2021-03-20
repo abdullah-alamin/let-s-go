d0d6d5ad47225d73125a46ff94ded5a0cd8ee2c4
abdullah-alamin <abdullah.prohor@gmail.com>
2021-03-20 23:48:52 +0600

    last

--- src/component/Map/Map.js
+++ src/component/Map/Map.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import map from '../../images/Map.png'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 function Map() {
+    const mapStyles = {        
+        height: "100vh",
+        width: "100%"};
+      
+      const defaultCenter = {
+        lat: 41.3851, lng: 2.1734
+      }
+      
     return (
         <div className='booking-map'>
             <img src={map} alt=""/>
+            {/* <LoadScript
+                googleMapsApiKey='YOUR_API_KEY_HERE'> ----I couldn't manage the api key----
+                <GoogleMap
+                mapContainerStyle={mapStyles}
+                zoom={13}
+                center={defaultCenter}
+                />
+            </LoadScript> */}
         </div>
     )
 }
 
 export default Map
+​
+const MapContainer = () => {
+  
+ 
+  return (
+     
+  )
+}
+​
+export default MapContainer;
